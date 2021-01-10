d9e2816e2defd899ce2460bf21bfe82048383476
Paweł Tymosiak <szozr2@gmail.com>
2021-01-10 17:55:43 +0100

    Created basic flask API Created basic flask app with hardcoded data. Added gitignore and Pipfile.

---
+++ main.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, abort
+
+from static_bgs import bgs
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def show_docs():
+    return 'Available endpoint: /boardgames/'
+
+
+@app.route('/boardgames')
+def get_all_boardgames():
+    return jsonify(bgs)
+
+
+@app.route('/boardgames/<int:bg_id>')
+def get_boardgame(bg_id):
+    try:
+        bg = bgs[bg_id]
+        return jsonify(bg)
+    except IndexError:
+        return abort(404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
