90ddeec1f553f76dce0f93e5137679003402c274
Paweł Tymosiak <szozr2@gmail.com>
2021-01-21 17:03:26 +0100

    Changed data source to database Changed data source to PostgreSQL database and added ORM.

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
-from flask import Flask, jsonify, abort
+from flask import Flask, jsonify
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from os import environ
 
-from static_bgs import bgs
+from models import Game
 
 app = Flask(__name__)
 CORS(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DATABASE_URL')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 
 @app.route('/')
@@ -16,18 +22,12 @@ def show_docs():
 @app.route('/boardgames')
 def get_all_boardgames():
     """Return list of dicts with all boardgames data."""
-    return jsonify(bgs)
+    query_bgs = [game.to_dict() for game in Game.query.all()]
+    return jsonify(query_bgs)
 
 
 @app.route('/boardgames/<int:bg_id>')
 def get_boardgame(bg_id):
     """Return dict with boardgame data based on given id."""
-    try:
-        bg = bgs[bg_id]
-        return jsonify(bg)
-    except IndexError:
-        return abort(404)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    game = Game.query.filter_by(id=bg_id).first_or_404()
+    return jsonify(game.to_dict())
